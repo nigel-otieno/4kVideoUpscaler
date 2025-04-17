f718dd31572809d1638abbb163fdac8a86111ab8
Nigel Otieno <nigelotieno@Nigels-MacBook-Pro.local>
2025-04-16 23:41:14 -0700

    initial commit

--- upscale_app/app.py
+++ upscale_app/app.py
@@ -120,4 +120,5 @@ def download_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
